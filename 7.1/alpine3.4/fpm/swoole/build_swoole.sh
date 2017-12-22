2e4d23d16b21784725f21f1ac982cbe3e78c04bc
zacksleo <zacksleo@gmail.com>
2017-12-22 16:14:35 +0800

    fix swoole build bug

--- 7.1/alpine3.4/fpm/swoole/build_swoole.sh
+++ 7.1/alpine3.4/fpm/swoole/build_swoole.sh
@@ -7,20 +7,20 @@ cd /root/swoole-src
 phpize
 ./configure --enable-openssl
 make && make install
-#apk del libaio-dev php-dev git autoconf build-base linux-headers pcre-dev openssl-dev
-apk del --no-cache php-dev
-apk del --no-cache git
-apk del --no-cache build-base
-apk del --no-cache make
+apk del libaio-dev php-dev git autoconf build-base linux-headers pcre-dev
+ apk del --no-cache php-dev
+ apk del --no-cache git
+ apk del --no-cache build-base
+ apk del --no-cache make
 apk del --no-cache openssl-dev
-apk del --no-cache linux-headers
-apk del --no-cache libaio-dev
-apk del --no-cache pcre-dev
-apk del --no-cache autoconf
-apk del --no-cache .persistent-deps
-apk del --no-cache libmcrypt-dev
-apk del --no-cache g++
-#apk del --no-cache icu-dev
+ apk del --no-cache linux-headers
+ apk del --no-cache libaio-dev
+ apk del --no-cache pcre-dev
+ apk del --no-cache autoconf
+ apk del --no-cache .persistent-deps
+ apk del --no-cache libmcrypt-dev
+ apk del --no-cache g++
+# apk del --no-cache icu-dev
 apk info
 php -m
 rm -rf /var/cache/apk/*
