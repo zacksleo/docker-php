2067011eaf1787f3dfd5a734569c3bfbec8831f0
zacksleo <zacksleo@gmail.com>
2018-01-08 16:57:49 +0800

    fix(build): 修改swoole安装方式, 改为使用pecl安装

--- 7.1/alpine3.4/fpm/swoole/build_swoole.sh
+++ 7.1/alpine3.4/fpm/swoole/build_swoole.sh
@@ -1,12 +1,16 @@
-apk add git autoconf build-base linux-headers libaio-dev pcre-dev openssl-dev icu-dev
-ln -s /usr/bin/php-config7 /usr/bin/php-config
-ln -s /usr/bin/phpize7 /usr/bin/phpize
-cd /root/
-git clone https://github.com/swoole/swoole-src.git
-cd /root/swoole-src
-phpize
-./configure --enable-openssl
-make && make install
+apk update && apk add git autoconf build-base linux-headers libaio-dev pcre-dev openssl-dev icu-dev
+# ln -s /usr/bin/php-config7 /usr/bin/php-config
+# ln -s /usr/bin/phpize7 /usr/bin/phpize
+
+# cd /root/
+# git clone https://github.com/swoole/swoole-src.git
+# cd /root/swoole-src
+# phpize
+#./configure --enable-openssl
+# make && make install
+
+pecl install swoole-2.0.12 && docker-php-ext-enable swoole
+
 apk del libaio-dev php-dev git autoconf build-base linux-headers pcre-dev
  apk del --no-cache php-dev
  apk del --no-cache git
